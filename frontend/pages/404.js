9906636cda90bbe96befc5f6b4b4429ecd673c14
Zalotleh <92803581+Zalotleh@users.noreply.github.com>
2023-01-10 14:19:03 +0100

    added the following: - search jobs with keyword and location - adding pagination - filters component - setting up filter in query

--- frontend/pages/404.js
+++ frontend/pages/404.js
@@ -2,9 +2,6 @@ import NotFound from "../components/layout/NotFound";
 import layout from "../components/layout/layout";
 
 export default function NotFoundPage() {
-  return (
-  <layout>
-    <NotFound />
-  </layout>
-  )
+  return  <NotFound />;
+  
 }
