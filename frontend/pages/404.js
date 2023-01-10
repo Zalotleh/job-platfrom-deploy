ee3e7de3b5168a473995235d1206129b234a2fe7
Zalotleh <92803581+Zalotleh@users.noreply.github.com>
2023-01-10 10:35:11 +0100

    added the following: - created Next.js app - job-platform components details - header and footer and loader components - layout component - homepage -data fetching from backend - get all jobs with getServerSideProps - render jobs and jobsitem components - get jobsdetails - render job details - create not found component

---
+++ frontend/pages/404.js
@@ -0,0 +1,10 @@
+import NotFound from "../components/layout/NotFound";
+import layout from "../components/layout/layout";
+
+export default function NotFoundPage() {
+  return (
+  <layout>
+    <NotFound />
+  </layout>
+  )
+}
