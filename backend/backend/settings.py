a4d7005beec5125effbca93b4cad6671b41dde15
Zalotleh <92803581+Zalotleh@users.noreply.github.com>
2023-01-19 11:53:32 +0100

    added the right allowed host in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'job-platform-v1.vercel.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'job-platform-django.herokuapp.com']
 
 # Application definition
 
