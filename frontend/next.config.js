e08a5fb2eaea4143c651e446a51b7f3d3e79ccb5
Zalotleh <92803581+Zalotleh@users.noreply.github.com>
2023-01-23 11:20:56 +0100

    try deploying on aws beanstalk

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
 //    API_URL: 'http://127.0.0.1:8000/',
-    API_URL: 'https://job-platform-django.herokuapp.com',
+//    API_URL: 'https://job-platform-django.herokuapp.com',
+    API_URL: 'http://jobplatform-env-1.eba-kixa3pu9.us-east-1.elasticbeanstalk.com'
   },
 };
 
