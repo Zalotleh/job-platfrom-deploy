b33d4498a491cf7cd8111b545a08f04737fb6ea3
Zalotleh <92803581+Zalotleh@users.noreply.github.com>
2023-01-25 10:19:04 +0100

    a

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -151,7 +151,8 @@ CORS_ALLOWED_ORIGINS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
